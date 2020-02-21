a5471047747df7b33ee65dc485d1174810adca78
Lev Brouk <lev@mattermost.com>
2020-02-20 20:50:53 -0800

    rotation debug-delete test

--- server/command/rotation_archive_test.go
+++ server/command/rotation_archive_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/mattermost/mattermost-plugin-solar-lottery/server/sl"
+	"github.com/mattermost/mattermost-plugin-solar-lottery/server/utils/kvstore"
 	"github.com/mattermost/mattermost-plugin-solar-lottery/server/utils/types"
 )
 
@@ -41,12 +42,62 @@ func TestCommandRotationArchive(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, types.NewSet("test", "test-345"), activeRotations)
 
-		// store.Entity(sl.KeyRotation).Load()
+		err = store.Entity(sl.KeyRotation).Load("test-123", &r)
+		require.NoError(t, err)
+		require.Equal(t, &sl.Rotation{
+			RotationID: "test-123",
+			IsArchived: true,
+		}, r)
 
 		rr := []string{}
 		_, err = runJSONCommand(t, SL, `
 			/lotto rotation list`, &rr)
 		require.NoError(t, err)
 		require.Equal(t, []string{"test", "test-345"}, rr)
+
+		_, err = runCommand(t, SL, `
+			/lotto rotation show test-123`)
+		require.Equal(t, kvstore.ErrNotFound, err)
 	})
+
+}
+
+func TestCommandRotationDelete(t *testing.T) {
+	t.Run("happy", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		SL, store := getTestSL(t, ctrl)
+
+		runCommands(t, SL, `
+			/lotto rotation new test
+			/lotto rotation new test-123
+			/lotto rotation new test-345
+			`)
+
+		r := &sl.Rotation{}
+		_, err := runJSONCommand(t, SL, `
+			/lotto rotation debug-delete test-123`, &r)
+		require.NoError(t, err)
+		require.Equal(t, &sl.Rotation{
+			RotationID: "test-123",
+		}, r)
+
+		activeRotations, err := store.Index(sl.KeyActiveRotations).Load()
+		require.NoError(t, err)
+		require.Equal(t, types.NewSet("test", "test-345"), activeRotations)
+
+		err = store.Entity(sl.KeyRotation).Load("test-123", &r)
+		require.Equal(t, kvstore.ErrNotFound, err)
+
+		rr := []string{}
+		_, err = runJSONCommand(t, SL, `
+			/lotto rotation list`, &rr)
+		require.NoError(t, err)
+		require.Equal(t, []string{"test", "test-345"}, rr)
+
+		_, err = runCommand(t, SL, `
+			/lotto rotation show test-123`)
+		require.Equal(t, kvstore.ErrNotFound, err)
+	})
+
 }
