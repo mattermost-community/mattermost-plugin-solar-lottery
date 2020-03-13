f172cd9212a7c9c3fe45344bfdbd55c16220f5ce
Lev Brouk <lev@mattermost.com>
2020-03-13 14:37:17 -0700

    Misc cleanup

---
+++ server/sl/filler/queue/task_filler.go
@@ -0,0 +1,25 @@
+// Copyright (c) 2019-present Mattermost, Inc. All Rights Reserved.
+// See License for license information.
+
+package queue
+
+import (
+	"github.com/pkg/errors"
+
+	sl "github.com/mattermost/mattermost-plugin-solar-lottery/server/sl"
+	"github.com/mattermost/mattermost-plugin-solar-lottery/server/utils/bot"
+)
+
+const Type = "queue"
+
+type taskFiller struct{}
+
+var _ sl.TaskFiller = (*taskFiller)(nil)
+
+func New() sl.TaskFiller {
+	return &taskFiller{}
+}
+
+func (*taskFiller) FillTask(r *sl.Rotation, task *sl.Task, logger bot.Logger) (*sl.Users, error) {
+	return nil, errors.New("Queue autofill is not implemented")
+}
